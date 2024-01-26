945519083833475cac3f19fd87c93e4ac443b7fc
PashechkinP <xeunique@mail.ru>
2024-01-26 20:17:28 +0300

    Mui component AdCard added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
-import Registration from './components/registration';
+import AdCard from './components/adCard.js';
 
 function App() {
   return (
     <div>
-      <Registration />
+      <AdCard
+      imgLink={"https://jjji.ru/1920x1280"}
+      adTitle={"This is Title"}
+      adText={"This is some text, yeeeee baby"}
+      />
     </div>
   );
 }
--- src/components/adCard.js
+++ src/components/adCard.js
@@ -1,16 +1,37 @@
-import Card from 'react-bootstrap/Card';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import "./regform.css"
 
 export default function AdCard({imgLink,adTitle,adText}) {
   return (
-      <div>
-          <Card style={{ width: '25rem' }} bg = 'light' border="success">
-              <Card.Img variant="top" src={imgLink} />
-              <Card.Body>
-                  <Card.Title>{adTitle}</Card.Title>
-                  <Card.Text>{adText}</Card.Text>
-              </Card.Body>
-          </Card>
-      </div>
-
+    <div className='container'>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image= {imgLink}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {adTitle}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {adText}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          comment
+        </Button>
+      </CardActions>
+    </Card>
+    </div>
   );
 }
